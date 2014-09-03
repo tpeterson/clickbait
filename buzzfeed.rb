2574c81f7262c434183c5cacad1585b51de53354
Tim <tpeterson.la@gmail.com>
2014-09-03 10:51:27 -0700

    Added BuzzFeed
    Added a script that pulls headlines from BuzzFeed’s homepage.

---
+++ buzzfeed.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'csv'
+
+URL_FILE = "headlines/buzzfeed.txt"
+headlines = []
+
+url = 'http://www.buzzfeed.com/'
+
+page = Nokogiri::HTML(open(url))
+
+page.css('.homepage-columns h2 a').each do |link|
+  headlines << link.inner_html
+end
+
+File.open(URL_FILE, 'w') do |f|
+  f.puts Time.now
+  f.puts headlines
+end
+
+puts "Copied #{headlines.length} headlines"
